05e6364b15cd0981c83fbcde52eed390e64af89a
looliby <2313210726@qq.com>
2021-11-17 19:05:12 +0800

    Site updated: 2021-11-17 19:05:12

--- js/src/post-details.js
+++ js/src/post-details.js
@@ -73,6 +73,8 @@ $(document).ready(function () {
   $('.post-toc a').on('click', function (e) {
     e.preventDefault();
     var targetSelector = NexT.utils.escapeSelector(this.getAttribute('href'));
+    targetSelector = decodeURI(this.getAttribute('href'))
+    var offset = $(targetSelector).offset().top;
     var offset = $(targetSelector).offset().top;
 
     hasVelocity ?
